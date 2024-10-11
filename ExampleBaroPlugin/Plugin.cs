6a6e913e5b9365ecb57e28f9cd8b7eb1cd320569
Markus Isberg <3e849f2e5c@pm.me>
2024-10-11 17:24:35 +0300

    Initial Commit

---
+++ ExampleBaroPlugin/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BarotraumaModInterface;
+
+public class Plugin : IBarotraumaPlugin
+{
+    [PluginService]
+    public static IDebugConsole DebugConsole { get; private set; } = null!;
+
+    public void Init()
+    {
+        DebugConsole.NewMessage("Plugin loaded");
+    }
+
+    public void Dispose()
+    {
+        DebugConsole.NewMessage("Plugin unloaded");
+    }
+}
