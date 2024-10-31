7af8086d2e834edbbab6167c1472a64d371b9a82
Markus Isberg <markus@fakefishgames.com>
2024-10-31 12:11:38 +0200

    PluginToolbox now works on Linux

--- PluginToolbox/Program.cs
+++ PluginToolbox/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace PluginToolbox;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace PluginToolbox;
 
 internal static class Program
 {
-    // PluginToolbox/bin/Debug/net.6.0 = 4 directories deep
-    private static readonly string projectRoot = Path.Combine("..", "..", "..", "..");
+    private static string projectRoot
+    {
+        get
+        {
+            // try getting the project directory from the assembly metadata
+            Assembly asm = typeof(Program).Assembly;
+            var attrs = asm.GetCustomAttributes<AssemblyMetadataAttribute>();
+            string? projectDir = attrs.FirstOrDefault(static a => a.Key == "ProjectDir")?.Value;
+
+            // no metadata? just hardcode the path I guess
+            if (string.IsNullOrWhiteSpace(projectDir))
+            {
+                // PluginToolbox/bin/Debug/net.6.0 = 4 directories deep
+                return Path.Combine("..", "..", "..", "..");
+            }
+            return Path.Combine(projectDir, "..");
+        }
+    }
+
+    public static bool IsWindows()
+        => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+    public static bool IsMacOS()
+        => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+    public static bool IsLinux()
+        => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
 
     internal static void Main(string[] args)
     {
@@ -28,6 +54,14 @@ internal static class Program
 
     private static void Build()
     {
+        string prefix = (IsWindows(), IsMacOS(), IsLinux()) switch
+        {
+            (true, _, _) => "Windows",
+            (_, true, _) => "Mac",
+            (_, _, true) => "Linux",
+            _            => throw new PlatformNotSupportedException()
+        };
+
         string buildPath = Path.Combine(Directory.GetCurrentDirectory(), "Build");
         if (Directory.Exists(buildPath))
         {
@@ -48,22 +82,22 @@ internal static class Program
         Directory.CreateDirectory(clientPath);
 
         DotnetCmd.CompileProject(
-            projectPath: Path.Combine(projectRoot, "ClientSource", "WindowsClient.csproj"),
+            projectPath: Path.Combine(projectRoot, "ClientSource", $"{prefix}Client.csproj"),
             configuration: Configuration.Debug,
             runtime: Runtime.Windows,
             outPath: clientPath);
 
-        builder.AddAssembly(Path.Combine(clientPath, "WindowsClient.dll"));
+        builder.AddAssembly(Path.Combine(clientPath, $"{prefix}Client.dll"));
 
         string serverPath = Path.Combine(modDir, "Server");
         Directory.CreateDirectory(serverPath);
         DotnetCmd.CompileProject(
-            projectPath: Path.Combine(projectRoot, "ServerSource", "WindowsServer.csproj"),
+            projectPath: Path.Combine(projectRoot, "ServerSource", $"{prefix}Server.csproj"),
             configuration: Configuration.Debug,
             runtime: Runtime.Windows,
             outPath: serverPath);
 
-        builder.AddAssembly(Path.Combine(serverPath, "WindowsServer.dll"));
+        builder.AddAssembly(Path.Combine(serverPath, $"{prefix}Server.dll"));
         builder.Build();
     }
 
