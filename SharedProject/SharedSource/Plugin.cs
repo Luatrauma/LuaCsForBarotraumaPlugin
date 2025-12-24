056834678acca7a68f1b7f1afeb8c4015921e56b
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2025-12-23 21:47:49 -0300

    Some testing with Lua

--- SharedProject/SharedSource/Plugin.cs
+++ SharedProject/SharedSource/Plugin.cs
@@ -1,21 +1,29 @@
 ﻿using Barotrauma;
 using Barotrauma.Plugins;
 using Microsoft.Xna.Framework;
+using MoonSharp.Interpreter;
 
 namespace LuaCsForBarotrauma;
 
 public partial class Plugin : IBarotraumaPlugin
 {
     public static readonly IDebugConsole DebugConsole = PluginServiceProvider.GetService<IDebugConsole>();
+    private Script? script;
 
     public void Init()
-    {
+    { 
         DebugConsole.NewMessage("LuaCsForBarotrauma loaded", Color.Lime);
+
+        Script script = new Script();
+        UserData.RegisterType(typeof(IDebugConsole));
+        script.Globals["DebugConsole"] = UserData.Create(DebugConsole);
+        script.DoString("DebugConsole.NewMessage('hi')");
     }
 
     public void Dispose() 
     { 
-        DebugConsole.NewMessage("LuaCsForBarotrauma unloaded", Color.Red); 
+        script = null;
+        DebugConsole.NewMessage("LuaCsForBarotrauma unloaded", Color.Red);
     }
 
     public void OnContentLoaded()
