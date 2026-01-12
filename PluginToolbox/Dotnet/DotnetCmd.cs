2638b8328541053678f84a415a9afef817f0c07d
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2026-01-12 17:05:46 -0300

    Bulldoze the plugin toolbox project and add a command that builds the projects for all platforms

--- PluginToolbox/Dotnet/DotnetCmd.cs
+++ PluginToolbox/Dotnet/DotnetCmd.cs
@@ -5,99 +5,16 @@ namespace PluginToolbox;
 
 internal static class DotnetCmd
 {
-    private static readonly Version DesiredRuntimeVersion = new(6, 0, 8);
+    private const string DesiredRuntimeVersion = "8.0.0";
 
-    private static string? FindDotNet()
-    {
-        // check if dotnet is in PATH
-        string globalDotNetPath = Where("dotnet");
-        if (!string.IsNullOrWhiteSpace(globalDotNetPath))
-        {
-            // we have one, let's see if it has the desired runtime
-            if (HasDesiredRuntime(DotNetListSdks(globalDotNetPath)))
-            {
-                // bingo!
-                return globalDotNetPath;
-            }
-
-            // nope, let's keep looking, maybe there's another dotnet installation with the desired runtime
-        }
-
-        // ~/.dotnet/dotnet on Unix, %USERPROFILE%\.dotnet\dotnet.exe on Windows
-        string homeDotNetPath = Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-            ".dotnet",
-            Program.IsWindows() ? "dotnet.exe" : "dotnet");
-
-        // maybe it's here?
-        if (File.Exists(homeDotNetPath))
-        {
-            // let's see if it has the desired runtime
-            if (HasDesiredRuntime(DotNetListSdks(homeDotNetPath)))
-            {
-                return homeDotNetPath;
-            }
-        }
-
-        // yeah, no luck
-        return null;
-
-        static string[] DotNetListSdks(string path)
-        {
-            string output = RunCommand(path, "--list-sdks");
-            return output.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-        }
-
-        static bool HasDesiredRuntime(string[] sdks)
-        {
-            foreach (string sdk in sdks)
-            {
-                // 6.0.8 [/path/to/sdk]
-                string[] parts = sdk.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-                if (parts.Length < 2) { throw new Exception($"Unexpected output from dotnet --list-sdks: {sdk}"); }
-
-                string version = parts[0];
-                if (!Version.TryParse(version, out Version? sdkVersion)) { continue; }
-
-                if (sdkVersion.Major == DesiredRuntimeVersion.Major &&
-                    sdkVersion.Minor == DesiredRuntimeVersion.Minor)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        static string Where(string command)
-            => RunCommand(Program.IsWindows() ? "where.exe" : "which", command);
-
-        static string RunCommand(string command, string arg)
-        {
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-                FileName = command,
-                ArgumentList = { arg },
-                RedirectStandardOutput = true
-            };
-
-            var process = Process.Start(psi) ?? throw new Exception("Failed to start process");
-            process.WaitForExit();
-
-            string output = process.StandardOutput.ReadToEnd().Trim();
-            return output;
-        }
-    }
-
-    public static void CompileProject(string projectPath, Configuration configuration,
-                                      Runtime runtime, string outPath)
+    public static void CompileProject(string projectPath, Configuration configuration, Runtime runtime)
     {
         ProcessStartInfo psi = new ProcessStartInfo
         {
-            FileName = FindDotNet() ?? throw new Exception("dotnet not found"),
+            FileName = "dotnet",
             ArgumentList =
             {
-                "publish",
+                "build",
                 projectPath,
                 "-c",
                 configuration.ToString(),
@@ -105,9 +22,7 @@ internal static class DotnetCmd
                 "-r",
                 runtime.Identifier,
                 "/p:Platform=AnyCPU",
-                $"/p:RuntimeFrameworkVersion={DesiredRuntimeVersion.Major}.{DesiredRuntimeVersion.Minor}.{DesiredRuntimeVersion.Build}",
-                "-o",
-                outPath
+                $"/p:RuntimeFrameworkVersion={DesiredRuntimeVersion}"
             },
             RedirectStandardOutput = true,
             RedirectStandardError = true
--- PluginToolbox/Program.cs
+++ PluginToolbox/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Xml.Linq;
@@ -21,7 +22,6 @@ internal static class Program
             // no metadata? just hardcode the path I guess
             if (string.IsNullOrWhiteSpace(projectDir))
             {
-                // PluginToolbox/bin/Debug/net.6.0 = 4 directories deep
                 return Path.Combine("..", "..", "..", "..");
             }
 
@@ -38,6 +38,15 @@ internal static class Program
     public static bool IsLinux()
         => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
 
+    private static string GetPlatformString()
+        => (IsWindows(), IsMacOS(), IsLinux()) switch
+        {
+            (true, _, _) => "Windows",
+            (_, true, _) => "Mac",
+            (_, _, true) => "Linux",
+            _ => throw new PlatformNotSupportedException()
+        };
+
     internal static void Main(string[] args)
     {
         foreach (string arg in args)
@@ -47,140 +56,19 @@ internal static class Program
                 case "--build":
                     Build();
                     break;
-                case "--initial-setup":
-                    InitialSetup();
-                    break;
-                case "--update-metadata":
-                    UpdateMetadata();
-                    break;
             }
         }
-    }
 
-    private static void InitialSetup()
-    {
-        string modName = AskForInput("Enter the mod name") ?? string.Empty;
-        if (!IsValid(modName))
+        string? action = AskForInput("What to do? [build]");
+        if (action == null) { return; }
+
+        switch (action)
         {
-            Console.WriteLine("Mod name cannot be empty");
-            return;
-        }
-
-        string author = AskForInput("Enter the author(s) name") ?? string.Empty;
-        if (!IsValid(author))
-        {
-            Console.WriteLine("Author name cannot be empty");
-            return;
-        }
-
-        string repo = AskForInput("Enter the repository URL (leave blank for none)") ?? string.Empty;
-
-        (XDocument Doc, string Path)[] projects = GetCsprojFilesToUpdate();
-
-        (XDocument Doc, string Path) currentPlatformProject =
-            projects.First(static p => Path.GetFileName(path: p.Path).Contains(GetPlatformString()));
-        Version gameVersion = GetGameVersionFromCsproj(currentPlatformProject.Doc, currentPlatformProject.Path);
-
-        foreach (var (csproj, path) in projects)
-        {
-            Csproj.SetVersion(csproj, new Version(major: 1, minor: 0, build: 0));
-            Csproj.SetAssemblyName(csproj, modName);
-            Csproj.SetRootNamespace(csproj, FormatIdentifier(modName, string.Empty, allowNumbers: false));
-            Csproj.SetAuthors(csproj, author);
-
-            Csproj.SetBaroMetadata(csproj,
-                                   Metadata.GameVersion(gameVersion),
-                                   Metadata.RepositoryUrl(repo));
-            Csproj.SaveCsproj(csproj, path);
-        }
-
-        static bool IsValid(string? answer)
-            => !string.IsNullOrWhiteSpace(answer);
-
-        static string FormatIdentifier(string id, string sub, bool allowNumbers)
-        {
-            StringBuilder sb = new();
-            foreach (char c in id)
-            {
-                if (asciiLetters.Contains(c) || (allowNumbers && asciiDigits.Contains(c)))
-                {
-                    sb.Append(c);
-                }
-                else
-                {
-                    sb.Append(sub);
-                }
-            }
-
-            return sb.ToString();
+            case "build":
+                Build();
+                break;
         }
     }
-
-    private static Version GetGameVersionFromCsproj(XDocument csproj, string path)
-    {
-        string oldDir = Directory.GetCurrentDirectory();
-        string? assemblyPath = Csproj.GetBaroAssemblyPath(csproj);
-
-        if (assemblyPath is null)
-        {
-            throw new InvalidOperationException("Could not find assembly path in csproj");
-        }
-
-        Directory.SetCurrentDirectory(Path.GetDirectoryName(path)!);
-        AssemblyName name = AssemblyName.GetAssemblyName(assemblyPath);
-        Directory.SetCurrentDirectory(oldDir);
-        return name.Version!;
-    }
-
-    private static readonly char[] asciiLetters
-        = Enumerable.Range('A', 'Z').Concat(Enumerable.Range('a', 'z'))
-                    .Select(static i => (char)i)
-                    .ToArray();
-
-    private static readonly char[] asciiDigits
-        = Enumerable.Range('0', '9')
-                    .Select(static i => (char)i)
-                    .ToArray();
-
-    public static (XDocument Doc, string Path)[] GetCsprojFilesToUpdate()
-    {
-        string clientPath = Path.Combine(projectRoot, "ClientSource");
-        string serverPath = Path.Combine(projectRoot, "ServerSource");
-
-        string[] clientCsprojs = Directory.GetFiles(clientPath, "*Client.csproj", SearchOption.TopDirectoryOnly);
-        string[] serverCsprojs = Directory.GetFiles(serverPath, "*Server.csproj", SearchOption.TopDirectoryOnly);
-
-        List<(XDocument, string)> docs = new();
-        docs.AddRange(clientCsprojs.Select(static path => (Csproj.ParseCsproj(path), path)));
-        docs.AddRange(serverCsprojs.Select(static path => (Csproj.ParseCsproj(path), path)));
-
-        return docs.ToArray();
-    }
-
-    private static void UpdateMetadata()
-    {
-        (XDocument Doc, string Path)[] projects = GetCsprojFilesToUpdate();
-
-        (XDocument Doc, string Path) currentPlatformProject =
-            projects.First(static c => Path.GetFileName(path: c.Path).Contains(GetPlatformString()));
-
-        Version gameVersion = GetGameVersionFromCsproj(currentPlatformProject.Doc, currentPlatformProject.Path);
-
-        foreach (var (csproj, path) in projects)
-        {
-            Csproj.SetBaroMetadata(csproj, Metadata.GameVersion(gameVersion));
-            Csproj.SaveCsproj(csproj, path);
-        }
-    }
-
-    private static string GetPlatformString()
-        => (IsWindows(), IsMacOS(), IsLinux()) switch
-        {
-            (true, _, _) => "Windows",
-            (_, true, _) => "Mac",
-            (_, _, true) => "Linux",
-            _            => throw new PlatformNotSupportedException()
-        };
 
     private static void Build()
     {
@@ -192,46 +80,22 @@ internal static class Program
             Directory.Delete(buildPath, recursive: true);
         }
 
-        Directory.CreateDirectory(buildPath);
+        List<(string ProjectPath, Runtime Runtime)> projects = [
+            ($@"{projectRoot}\ClientProject\WindowsClient.csproj", Runtime.Windows),
+            ($@"{projectRoot}\ClientProject\LinuxClient.csproj", Runtime.Linux),
+            ($@"{projectRoot}\ClientProject\MacClient.csproj", Runtime.Mac),
+            ($@"{projectRoot}\ServerProject\WindowsServer.csproj", Runtime.Windows),
+            ($@"{projectRoot}\ServerProject\LinuxServer.csproj", Runtime.Linux),
+            ($@"{projectRoot}\ServerProject\MacServer.csproj", Runtime.Mac)
+        ];
 
-        string modDir = Path.Combine(buildPath, "ExampleMod");
-        ContentPackageBuilder builder = new(
-            ModName: "ExampleMod",                                 // PLUGIN_TODO get mod name automatically
-            ModVersion: new Version(major: 1, minor: 0, build: 0), // PLUGIN_TODO get mod version automatically
-            GameVersion: new Version(major: 1, minor: 2, build: 8,
-                                     revision: 0), // PLUGIN_TODO get game version automatically
-            OutPath: modDir);
-        builder.Prepare();
+        foreach (var project in projects)
+        {
+            Console.WriteLine($"Building {project.ProjectPath}");
+            DotnetCmd.CompileProject(project.ProjectPath, Configuration.Release, project.Runtime);
+        }
 
-        string clientPath = Path.Combine(modDir, "Client");
-        Directory.CreateDirectory(clientPath);
-
-        string clientProjectPath = Path.Combine(projectRoot, "ClientSource", $"{prefix}Client.csproj");
-        DotnetCmd.CompileProject(
-            projectPath: clientProjectPath,
-            configuration: Configuration.Debug,
-            runtime: Runtime.Windows,
-            outPath: clientPath);
-
-        string clientAssemblyName =
-            $"{Csproj.GetAssemblyName(Csproj.ParseCsproj(clientProjectPath)) ?? $"{prefix}Client"}.dll";
-
-        builder.AddAssembly(ContentPackageBuilder.AssemblyType.Client, Path.Combine(clientPath, clientAssemblyName));
-
-        string serverPath = Path.Combine(modDir, "Server");
-        string serverProjectPath = Path.Combine(projectRoot, "ServerSource", $"{prefix}Server.csproj");
-        Directory.CreateDirectory(serverPath);
-        DotnetCmd.CompileProject(
-            projectPath: serverProjectPath,
-            configuration: Configuration.Debug,
-            runtime: Runtime.Windows,
-            outPath: serverPath);
-
-        string serverAssemblyName =
-            $"{Csproj.GetAssemblyName(Csproj.ParseCsproj(serverProjectPath)) ?? $"{prefix}Server"}.dll";
-
-        builder.AddAssembly(ContentPackageBuilder.AssemblyType.Server, Path.Combine(serverPath, serverAssemblyName));
-        builder.Build();
+        Console.WriteLine("Finished building!");
     }
 
     private static string? AskForInput(string prompt)
