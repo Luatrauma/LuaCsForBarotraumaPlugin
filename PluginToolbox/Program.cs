d330a1d7367e8056681ce439df04a2e6a12493ad
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2026-01-13 12:04:15 -0300

    Remove more unused stuff from PluginToolbox

--- PluginToolbox/Program.cs
+++ PluginToolbox/Program.cs
@@ -1,51 +1,14 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Xml.Linq;
-
-namespace PluginToolbox;
+﻿namespace PluginToolbox;
 
 internal static class Program
 {
-    private static string projectRoot
+    private static string ProjectRoot
     {
         get
         {
-            // try getting the project directory from the assembly metadata
-            Assembly asm = typeof(Program).Assembly;
-            string? projectDir = asm
-                                 .GetCustomAttributes<AssemblyMetadataAttribute>()
-                                 .FirstOrDefault(static a => a.Key == "SolutionRoot")
-                                 ?.Value;
-
-            // no metadata? just hardcode the path I guess
-            if (string.IsNullOrWhiteSpace(projectDir))
-            {
-                return Path.Combine("..", "..", "..", "..");
-            }
-
-            return projectDir;
+            return Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location)!, "..", "..", "..", "..");
         }
     }
-
-    public static bool IsWindows()
-        => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-
-    public static bool IsMacOS()
-        => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
-
-    public static bool IsLinux()
-        => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
-
-    private static string GetPlatformString()
-        => (IsWindows(), IsMacOS(), IsLinux()) switch
-        {
-            (true, _, _) => "Windows",
-            (_, true, _) => "Mac",
-            (_, _, true) => "Linux",
-            _ => throw new PlatformNotSupportedException()
-        };
 
     internal static void Main(string[] args)
     {
@@ -72,8 +35,6 @@ internal static class Program
 
     private static void Build()
     {
-        string prefix = GetPlatformString();
-
         string buildPath = Path.Combine(Directory.GetCurrentDirectory(), "Build");
         if (Directory.Exists(buildPath))
         {
@@ -81,12 +42,12 @@ internal static class Program
         }
 
         List<(string ProjectPath, Runtime Runtime)> projects = [
-            ($@"{projectRoot}\ClientProject\WindowsClient.csproj", Runtime.Windows),
-            ($@"{projectRoot}\ClientProject\LinuxClient.csproj", Runtime.Linux),
-            ($@"{projectRoot}\ClientProject\MacClient.csproj", Runtime.Mac),
-            ($@"{projectRoot}\ServerProject\WindowsServer.csproj", Runtime.Windows),
-            ($@"{projectRoot}\ServerProject\LinuxServer.csproj", Runtime.Linux),
-            ($@"{projectRoot}\ServerProject\MacServer.csproj", Runtime.Mac)
+            ($@"{ProjectRoot}\ClientProject\WindowsClient.csproj", Runtime.Windows),
+            ($@"{ProjectRoot}\ClientProject\LinuxClient.csproj", Runtime.Linux),
+            ($@"{ProjectRoot}\ClientProject\MacClient.csproj", Runtime.Mac),
+            ($@"{ProjectRoot}\ServerProject\WindowsServer.csproj", Runtime.Windows),
+            ($@"{ProjectRoot}\ServerProject\LinuxServer.csproj", Runtime.Linux),
+            ($@"{ProjectRoot}\ServerProject\MacServer.csproj", Runtime.Mac)
         ];
 
         foreach (var project in projects)
