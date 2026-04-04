c114969d3b8b480e1df9657ee16cddb047852402
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2026-04-04 20:23:22 -0300

    Add StatusEffectService and reimplement Lua hooks

---
+++ SharedProject/SharedSource/LuaCs/_Services/StatusEffectService.cs
@@ -0,0 +1,61 @@
+﻿using Barotrauma;
+using Barotrauma.LuaCs;
+using Barotrauma.Plugins;
+using System.Xml.Linq;
+
+internal class StatusEffectHookAction : IStatusEffectAction
+{
+    private readonly IEventService _eventService;
+    private readonly XElement _element;
+    private StatusEffect _statusEffect;
+    private readonly string _hookIdentifier;
+
+    public StatusEffectHookAction(XElement element, StatusEffect statusEffect, IEventService eventService)
+    {
+        _element = element;
+        _eventService = eventService;
+        _statusEffect = statusEffect;
+        _hookIdentifier = element.GetAttributeString("name", "");
+    }
+
+    public void Apply(StatusEffectParams effectParams)
+    {
+        _eventService.Call(_hookIdentifier, _statusEffect, effectParams.DeltaTime, effectParams.Entity, effectParams.Targets, effectParams.WorldPosition!, _element);
+    }
+}
+
+internal class StatusEffectService : ISystem, IStatusEffectActionFactory
+{
+    public bool IsDisposed { get; private set; }
+
+    private readonly IEventService _eventService;
+
+
+    public StatusEffectService(IEventService eventService)
+    {
+        _eventService = eventService;
+
+        Barotrauma.Plugins.PluginServiceProvider.GetService<IStatusEffectService>().RegisterAction(this);
+    }
+
+    public void Dispose()
+    {
+        IsDisposed = true;
+    }
+
+    public FluentResults.Result Reset()
+    {
+        return FluentResults.Result.Ok();
+    }
+
+    public bool SupportsElement(XElement element)
+    {
+        Identifier name = element.NameAsIdentifier();
+        return name == "hook" || name == "luahook";
+    }
+
+    public IStatusEffectAction Create(StatusEffect statusEffect, XElement element)
+    {
+        return new StatusEffectHookAction(element, statusEffect, _eventService);
+    }
+}
